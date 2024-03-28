42090ee7953cd7c935d3c7e8fda3e22912f6cf6e
indy brinkman <9021720@student.zadkine.nl>
2024-03-28 12:34:22 +0100

    begin gemaakt aan itnieuws page

--- home.php
+++ home.php
@@ -1,1 +1,12 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>uneed-it</title>
+</head>
+<body>
+
+</body>
+</html>
--- itnieuws.php
+++ itnieuws.php
@@ -1,1 +1,16 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>uneed-it</title>
+</head>
+<body>
+<nav>
+
+</nav>
+<h1>it nieuws page</h1>
+<p>op deze pagina kan je alle nieuws over it vinden.</p>
+</body>
+</html>
