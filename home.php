bccc9d3cc18c1a431f215eda94c299f5b191ca23
indy brinkman <9021720@student.zadkine.nl>
2024-03-28 14:07:17 +0100

    begonnen aan nav bar

--- home.php
+++ home.php
@@ -8,5 +8,6 @@
 </head>
 <body>
 
+</div>
 </body>
 </html>
--- itnieuws.php
+++ itnieuws.php
@@ -1,14 +1,23 @@
 <?php
 ?>
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>uneed-it</title>
+    <link rel="stylesheet" href="itnieuws.css"/>
 </head>
 <body>
 <nav>
-
+    <div class="nav">
+        <div class="navlogo">
+            <img src="logo.png" alt="uneed-it logo">
+        </div>
+        <a href="home.php">home</a>
+        <a href="itnieuws.php">it nieuws</a>
+        <a href="overons.php">over ons</a>
+        <a href="reparatie.php">reparatie</a>
+        <a href="service.php">service</a>
+        <a href="zakelijk.php">zakelijk</a>
 </nav>
 <h1>it nieuws page</h1>
 <p>op deze pagina kan je alle nieuws over it vinden.</p>
