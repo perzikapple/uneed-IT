5250583f7b1ae91c2e1590b65c27a1ad2e64eb4e
Boudewijnvandamme <Boudewijnvandamme@gmail.com>
2024-04-08 14:25:40 +0200

    info

--- overons.php
+++ overons.php
@@ -1,78 +1,148 @@
-<html>
+<!DOCTYPE html>
+<html lang="nl">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Over ons</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f5f5f5;
+            color: #333;
+        }
+
+        header {
+            background-color: #5e76d9;
+            color: white;
+            text-align: center;
+            padding: 1em;
+        }
+
+        nav ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 10px;
+        }
+
+        nav ul li a {
+            color: white;
+            text-decoration: none;
+        }
+
+        main {
+            padding: 20px;
+        }
+
+        .info {
+            margin-bottom: 20px;
+            border: solid 1px #ccc;
+        }
+
+        .sterke {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            padding: 20px;
+            background-color: #f9f9f9;
+            border-radius: 5px;
+        }
+
+        .sterke div {
+            flex: 1 0 48%;
+            margin-bottom: 20px;
+            padding: 20px;
+            background-color: #5e76d9;
+            color: white;
+            border-radius: 5px;
+        }
+
+        .sterke h2 {
+            margin-top: 0;
+        }
+
+        footer {
+            background-color: #5e76d9;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            width: 100%;
+            bottom: 0;
+        }
+        .info{
+            width: 50%;
+            float: left;
+        }
+        .sterke{
+            width: 45%;
+            float: right;
+            margin-top: 0px;
+            margin-right: -40px;
+        }
+
+    </style>
 </head>
 <body>
 <header>
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="zakelijk.php.php">zakelijk</a></li>
-            <li><a href="itnieuws.php"> nieuws</a></li>
-            <li><a href='reperaties'>reperaties</a></li>
-
+            <li><a href="zakelijk.php.php">Zakelijk</a></li>
+            <li><a href="itnieuws.php">Nieuws</a></li>
+            <li><a href='reparaties.php'>Reparaties</a></li>
         </ul>
     </nav>
 </header>
-<style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-    }
 
-    header {
-        background-color: #5e76d9;
-        color: white;
-        text-align: center;
-        padding: 1em;
-    }
-
-    nav ul {
-        list-style-type: none;
-        padding: 0;
-    }
-
-    nav ul li {
-        display: inline;
-        margin-right: 10px;
-    }
-
-    nav ul li a {
-        color: white;
-        text-decoration: none;
-    }
-
-    main {
-        padding: 1em;
-    }
-
-    footer {
-        background-color: #5e76d9;
-        color: white;
-        text-align: center;
-        padding: 1em;
-        position: absolute;
-        bottom: 0;
-        width: 97.8%;
-    }
-    #logo{
-        position: absolute;
-        left: 0px;
-        top: -10px;
-    }
-</style>
 <main>
-    <img id="logo" src="img/logo.png" alt width="300px" height="100px">
     <div class="info">
-        <h1>Over ons</h1>
-        <p>wij zijn een bedrijf die zich specializeert in het repareren van uw kapotte aparaten. wij makken alles van uw telefoon tot laptop of tablat.</p>
-
-        <a> Dit bedrijf is gemaakt voor afsprkaken op lacatie dus deze website is gemaakt om afspraken te maken en prijzen te ontdekken.</a>
+        <h3>JOHN EN PETRA SIPS</h3>
+        <p>John heeft van 1983 tot 1992 bij IBM Nederland gewerkt als operator en ontwikkeling van besturingssoftware, ook wel "OS2 Warp" genoemd. In die periode heb ik alle certificaten behaald die de basis vormen van de kennis die ik nu heb.</p>
+        <p>Ook via Microsoft in Nederland en in Amerika heb ik diverse certificaten behaald omdat ik onderscheidend wilde zijn ten opzichte van mijn collega's in de branche omdat wij willen dat onze klanten met netwerkbeheer of systeembeheer zo laag mogelijke kosten hebben per jaar. Dan heeft u als ondernemer geen frustraties meer omdat wij er alles aan doen om u tevreden te houden.</p>
+        <p>Ook bieden we zakelijke en particuliere abonnementen aan voor service & preventief onderhoud. We zijn sinds jaar en dag een begrip in de ruime regio van Waddinxveen en omstreken. Ons werkgebied loopt van Waddinxveen tot Zevenhuizen en Moerkapelle, Reeuwijk en Tempel maar ook in Zoetermeer, Rotterdam en Schiedam komt u ons regelmatig tegen. In Nieuw Vennep en Hoofddorp in de Haarlemmermeer heeft Uneed-IT een service point.</p>
     </div>
-    <a id="foto" <img src="eigenaar"></a>
+
+    <div class="sterke">
+        <div>
+            <h2>Reparatie binnen 1 dag</h2>
+            <p>Reparaties worden binnen een dag afgehandeld. 's Ochtends vroeg brengen is aan het eind van de dag weer ophalen. Mocht het onverhoopt toch langer duren dan ontvangt u kosteloos een vervangende laptop of computer.</p>
+        </div>
+
+        <div>
+            <h2>Lid van ICT Profs</h2>
+            <p>Wij zijn lid van de landelijk opererende vereniging 'ICT Profs'. Voor u is dat belangrijk omdat we binnen deze club van specialisten de innovaties op de voet volgen en kennis en knowhow met elkaar uitwisselen. Dit leidt niet zelden tot kortere reparatietijden en dus lagere kosten voor u.</p>
+        </div>
+
+        <div>
+            <h2>Werken AVG-proof</h2>
+            <p>Tijdens onze werkzaamheden staat uw veiligheid en privacy voorop. Wij houden ten alle tijden de normen van de AVG in acht en gaan zorgvuldig om met uw privé documenten.</p>
+        </div>
+
+        <div>
+            <h2>Nest Pro Installateur</h2>
+            <p>Sinds september 2019 zijn we NEST PRO Installateur. Door vakkennis en training in NEST producten, mag Uneed-IT dit voor u installeren. Daar zijn we trots op!</p>
+        </div>
+
+        <div>
+            <h2>Opslag in de Cloud</h2>
+            <p>We bieden diverse opslagmogelijkheden in de cloud op snelle, veilige servers. Dat kan zelfs separaat als back-up van uw eigen harddisk. Eigenlijk onontbeerlijk omdat goede beveiliging en preventie tegenwoordig echt belangrijk zijn.</p>
+        </div>
+
+        <div>
+            <h2>Heldere afspraken</h2>
+            <p>Wij zijn doeners en houden van heldere afspraken. Wij houden niet van bla bla of "We doen dit wel even". In "Jip en Janneke" taal laten wij zien wat we doen en wat wij voor u kunnen betekenen. Professionaliteit en betrouwbaarheid zijn onze standaard.</p>
+        </div>
+    </div>
 </main>
+
 <footer>
-    <p>&copy; 2021 UneedIT</p>
+    <p>&copy; 2024 UneedIT</p>
 </footer>
+</body>
 </html>
