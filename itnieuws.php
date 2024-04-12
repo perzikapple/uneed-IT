34a176c65be3366bcbedbe147d81671f72840995
indy brinkman <9021720@student.zadkine.nl>
2024-04-12 08:32:17 +0200

    bijna klaar met itnieuws pafe

--- itnieuws.php
+++ itnieuws.php
@@ -13,12 +13,12 @@
             <img src="logo.png" alt="uneed-it logo">
         </div>
         <div class="nav-links">
-        <a href="home.php">home</a>
-        <a href="itnieuws.php">it nieuws</a>
-        <a href="overons.php">over ons</a>
-        <a href="reparatie.php">reparatie</a>
-        <a href="service.php">service</a>
-        <a href="zakelijk.php">zakelijk</a>
+        <a href="home.php">Home</a>
+        <a href="itnieuws.php">It nieuws</a>
+        <a href="overons.php">Over ons</a>
+        <a href="reparatie.php">Reparatie</a>
+        <a href="service.php">Service</a>
+        <a href="zakelijk.php">Zakelijk</a>
         </div>
     </div>
 </nav>
@@ -36,36 +36,37 @@
     <div class="contact-section">
         <p>Neem contact met ons op:</p>
         <p>+316 30 985 409 servicenummer,
-            +3118 28 202 18 kantoor,
+            +3118 28 202 18 kantoor of email
             bereikbaar van 09:00 - 18:00</p>
     </div>
     <div class="contact-section">
-        <p>E-mail:</p>
-        <p> info@example.com</p>
+        <p>bezorg diensten:</p>
+        <p>UPS, DHL en HOMERR</p>
     </div>
 </div>
+<div class="kopje-itnieuws"><p>it nieuws </p></div>
 
     <div class="news-sections">
         <div class="news-section">
             <img src="itnieuws.jpg" alt="Nieuws 1">
             <div class="news-section-tekst">
-            <p>nieuws 1</p>
-            <p>een korten samenvatting</p>
+            <p>lees meer</p>
             </div>
+            <p>een korten samenvatting</p>
         </div>
         <div class="news-section">
             <img src="itnieuws.jpg" alt="Nieuws 2">
             <div class="news-section-tekst">
-            <p>nieuws 2</p>
-            <p>een korten samenvatting</p>
+            <p>lees meer</p>
             </div>
+            <p>een korten samenvatting</p>
         </div>
         <div class="news-section">
             <img src="itnieuws.jpg" alt="Nieuws 3">
             <div class="news-section-tekst">
-            <p>nieuws 3</p>
-            <p>een korten samenvatting</p>
+            <p>lees meer</p>
             </div>
+            <p>een korten samenvatting</p>
         </div>
     </div>
 
@@ -73,24 +74,23 @@
         <div class="news-section">
             <img src="itnieuws.jpg" alt="Nieuws 4">
             <div class="news-section-tekst">
-            <p>nieuws 4</p>
-            <p>een korten samenvatting</p>
+            <p>lees meer</p>
             </div>
+            <p>een korten samenvatting</p>
         </div>
         <div class="news-section">
             <img src="itnieuws.jpg" alt="Nieuws 5">
             <div class="news-section-tekst">
-            <p>nieuws 5</p>
-            <p>een korten samenvatting</p>
+            <p>lees meer</p>
             </div>
+            <p>een korten samenvatting</p>
         </div>
         <div class="news-section">
             <img src="itnieuws.jpg" alt="Nieuws 6">
             <div class="news-section-tekst">
-            <p>nieuws 6</p>
-            <p>een korten samenvatting</p>
+            <p>lees meer</p>
             </div>
-        </div>
+            <p>een korten samenvatting</p>
     </div>
 
 
