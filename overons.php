964df0e52d4c2a3c1d9095f6519b12226b513c19
Boudewijnvandamme <Boudewijnvandamme@gmail.com>
2024-04-12 12:43:19 +0200

    nieuw

--- overons.php
+++ overons.php
@@ -40,30 +40,10 @@
         }
 
         .info {
-            margin-bottom: 20px;
             border: solid 1px #ccc;
-        }
-
-        .sterke {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: space-between;
-            padding: 20px;
-            background-color: #f9f9f9;
             border-radius: 5px;
-        }
-
-        .sterke div {
-            flex: 1 0 48%;
-            margin-bottom: 20px;
-            padding: 20px;
-            background-color: #5e76d9;
-            color: white;
-            border-radius: 5px;
-        }
-
-        .sterke h2 {
-            margin-top: 0;
+            margin-right: 600px;
+            padding: 10px;
         }
 
         footer {
@@ -75,16 +55,105 @@
             width: 100%;
             bottom: 0;
         }
-        .info{
-            width: 50%;
+
+
+        .adres{
+            margin-top: 50px;
+            margin-left: 100px;
+        }
+
+        #locatie {
+            width: 30%;
             float: left;
+            padding: 20px;
+            padding-bottom: 54px;
+            background-color: #c12d29; /* Change this color to the desired background color */
         }
-        .sterke{
-            width: 45%;
-            float: right;
-            margin-top: 0px;
-            margin-right: -40px;
+
+        #berijkbaar {
+            width: 30%;
+            float: left;
+            padding: 20px;
+            border-left: solid 1px #ccc;
+            border-right: solid 1px #ccc;
+            background-color: #c12d29; /* Change this color to the desired background color */
         }
+
+        #abonnement {
+            width: 30%;
+            float: left;
+            padding: 20px;
+            background-color: #c12d29; /* Change this color to the desired background color */
+        }
+
+
+        #foto{
+            width: 300px;
+            height: 300px;
+            margin-top: -300px;
+            margin-left: 1000px;
+        }
+
+        .reperatie{
+            border: solid 1px #ccc;
+            position: absolute;
+            margin-top: 200px;
+            margin-right: 750px;
+            margin-left: 10px;
+            background-color: #0193aa;
+            border-radius: 10px
+        }
+
+        .ICT{
+            border: solid 1px #ccc;
+            position: absolute;
+            margin-top: 200px;
+            margin-right: 10px;
+            margin-left: 750px;
+            background-color: #0193aa;
+            border-radius: 10px;
+        }
+
+        #AVG{
+            border: solid 1px #ccc;
+            position: absolute;
+            margin-top: 400px;
+            margin-right: 750px;
+            margin-left: 10px;
+            background-color: #0193aa;
+            border-radius: 10px;
+        }
+
+        #instellateur{
+            border: solid 1px #ccc;
+            position: absolute;
+            margin-top: 400px;
+            margin-right: 10px;
+            margin-left: 750px;
+            background-color: #0193aa;
+            border-radius: 10px;
+        }
+
+        #cloud{
+            border: solid 1px #ccc;
+            position: absolute;
+            margin-top: 600px;
+            margin-right: 750px;
+            margin-left: 10px;
+            background-color: #0193aa;
+            border-radius: 10px;
+        }
+
+        #afspraken{
+            border: solid 1px #ccc;
+            position: absolute;
+            margin-top: 600px;
+            margin-right: 10px;
+            margin-left: 750px;
+            background-color: #0193aa;
+            border-radius: 10px;
+        }
+
 
     </style>
 </head>
@@ -108,41 +177,55 @@
         <p>Ook bieden we zakelijke en particuliere abonnementen aan voor service & preventief onderhoud. We zijn sinds jaar en dag een begrip in de ruime regio van Waddinxveen en omstreken. Ons werkgebied loopt van Waddinxveen tot Zevenhuizen en Moerkapelle, Reeuwijk en Tempel maar ook in Zoetermeer, Rotterdam en Schiedam komt u ons regelmatig tegen. In Nieuw Vennep en Hoofddorp in de Haarlemmermeer heeft Uneed-IT een service point.</p>
     </div>
 
-    <div class="sterke">
-        <div>
-            <h2>Reparatie binnen 1 dag</h2>
-            <p>Reparaties worden binnen een dag afgehandeld. 's Ochtends vroeg brengen is aan het eind van de dag weer ophalen. Mocht het onverhoopt toch langer duren dan ontvangt u kosteloos een vervangende laptop of computer.</p>
-        </div>
+    <img id="foto" src="img/eigenaar.jpeg">
 
-        <div>
-            <h2>Lid van ICT Profs</h2>
-            <p>Wij zijn lid van de landelijk opererende vereniging 'ICT Profs'. Voor u is dat belangrijk omdat we binnen deze club van specialisten de innovaties op de voet volgen en kennis en knowhow met elkaar uitwisselen. Dit leidt niet zelden tot kortere reparatietijden en dus lagere kosten voor u.</p>
+    <div class="adres">
+        <div id="locatie">
+            <p>Zuidbaan 514, 2841MD</p>
+            <p>Mordrecht</p>
         </div>
-
-        <div>
-            <h2>Werken AVG-proof</h2>
-            <p>Tijdens onze werkzaamheden staat uw veiligheid en privacy voorop. Wij houden ten alle tijden de normen van de AVG in acht en gaan zorgvuldig om met uw privé documenten.</p>
+        <div id="berijkbaar">
+            <p> +316 30 985 409 SERVICENUMMER</p>
+            <p> +3118 28 202 18 KANTOOR</p>
+            <p>BEREIKBAAR VAN 09:00 - 18:00</p>
         </div>
-
-        <div>
-            <h2>Nest Pro Installateur</h2>
-            <p>Sinds september 2019 zijn we NEST PRO Installateur. Door vakkennis en training in NEST producten, mag Uneed-IT dit voor u installeren. Daar zijn we trots op!</p>
-        </div>
-
-        <div>
-            <h2>Opslag in de Cloud</h2>
-            <p>We bieden diverse opslagmogelijkheden in de cloud op snelle, veilige servers. Dat kan zelfs separaat als back-up van uw eigen harddisk. Eigenlijk onontbeerlijk omdat goede beveiliging en preventie tegenwoordig echt belangrijk zijn.</p>
-        </div>
-
-        <div>
-            <h2>Heldere afspraken</h2>
-            <p>Wij zijn doeners en houden van heldere afspraken. Wij houden niet van bla bla of "We doen dit wel even". In "Jip en Janneke" taal laten wij zien wat we doen en wat wij voor u kunnen betekenen. Professionaliteit en betrouwbaarheid zijn onze standaard.</p>
+        <div id="abonnement">
+            <p> MA T/M VRIJ, 09:00 - 23:00</p>
+            <p>TELEFONISCH BEREIKBAAR</p>
+            <p>VOOR ABONNEMENTHOUDERS</p>
         </div>
     </div>
-</main>
 
-<footer>
-    <p>&copy; 2024 UneedIT</p>
-</footer>
+    <div class="reperatie">
+        <h2>Reparatie binnen 1 dag</h2>
+        <p>Reparaties worden binnen een dag afgehandeld. 's Ochtends vroeg brengen is aan het eind van de dag weer ophalen. Mocht het onverhoopt toch langer duren dan ontvangt u kosteloos een vervangende laptop of computer.</p>
+    </div>
+
+    <div class="ICT">
+        <h2>Lid van ICT Profs</h2>
+        <p>Wij zijn lid van de landelijk opererende vereniging 'ICT Profs'. Voor u is dat belangrijk omdat we binnen deze club van specialisten de innovaties op de voet volgen en kennis en knowhow met elkaar uitwisselen. Dit leidt niet zelden tot kortere reparatietijden en dus lagere kosten voor u.</p>
+    </div>
+
+    <div id="AVG">
+        <h2>Werken AVG-proof</h2>
+        <p>Tijdens onze werkzaamheden staat uw veiligheid en privacy voorop. Wij houden ten alle tijden de normen van de AVG in acht en gaan zorgvuldig om met uw privé documenten.</p>
+    </div>
+
+    <div id="instellateur">
+        <h2>Nest Pro Installateur</h2>
+        <p>Sinds september 2019 zijn we NEST PRO Installateur. Door vakkennis en training in NEST producten, mag Uneed-IT dit voor u installeren. Daar zijn we trots op!</p>
+    </div>
+
+    <div id="cloud">
+        <h2>Opslag in de Cloud</h2>
+        <p>We bieden diverse opslagmogelijkheden in de cloud op snelle, veilige servers. Dat kan zelfs separaat als back-up van uw eigen harddisk. Eigenlijk onontbeerlijk omdat goede beveiliging en preventie tegenwoordig echt belangrijk zijn.</p>
+    </div>
+
+    <div id="afspraken">
+        <h2>Heldere afspraken</h2>
+        <p>Wij zijn doeners en houden van heldere afspraken. Wij houden niet van bla bla of "We doen dit wel even". In "Jip en Janneke" taal laten wij zien wat we doen en wat wij voor u kunnen betekenen. Professionaliteit en betrouwbaarheid zijn onze standaard.</p>
+    </div>
+
+</main>
 </body>
 </html>
--- zakelijk.php
+++ zakelijk.php
@@ -4,14 +4,9 @@
 
 </head>
 <style>
-    body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-    }
 
     header {
-        background-color: #5e76d9;
+        background-color:  #00718d;
         color: white;
         text-align: center;
         padding: 1em;
@@ -36,49 +31,89 @@
         padding: 1em;
     }
 
-    footer {
-        background-color: #5e76d9;
-        color: white;
-        text-align: center;
-        padding: 1em;
-        position: absolute;
-        bottom: 0;
-        width: 97.8%;
-    }
     #logo{
         position: absolute;
         left: 0px;
         top: -10px;
     }
-    .info{
-        width: 50%;
-        float: left;
-    }
+
     .formulier{
-        width: 50%;
-        float: right;
+        position: absolute;
+        top: 100%;
+        left: 70%;
+        background: #0193aa;
+
     }
+
     form{
         margin: 0 auto;
-        width: 50%;
+        width: 70%;
     }
+
     label{
         display: block;
         margin-top: 10px;
     }
+
     input, textarea{
         width: 100%;
         padding: 10px;
         margin-top: 5px;
     }
+
     input[type="submit"]{
-        background-color: #5e76d9;
+        background-color: #c12d29;
         color: white;
         border: none;
         cursor: pointer;
     }
-    input[type="submit"]:hover{
-        background-color: #4a5ec2;
+
+    textarea{
+        height: 100px;
+    }
+
+    h2{
+        color: #5e76d9;
+    }
+
+    #text{
+        border: 3px solid #5e76d9;
+        padding: 10px;
+        background: ;
+    }
+
+    #text h2{
+        text-align: center;
+    }
+
+    #text p{
+        text-align: center;
+    }
+
+    .adres{
+        margin-top: 50px;
+        margin-left: 100px;
+        background-color: #c12d29;
+    }
+
+    #locatie{
+        width: 30%;
+        float: left;
+        padding: 20px;
+    }
+
+    #berijkbaar{
+        width: 30%;
+        float: left;
+        padding: 20px;
+        border-left: solid 1px #ccc;
+        border-right: solid 1px #ccc;
+    }
+
+    #abonnement{
+        width: 30%;
+        float: left;
+        padding: 20px;
     }
 
 </style>
@@ -97,66 +132,88 @@
 <main>
 
     <div class="info">
-        <h1>Zakelijk</h1>
-        <p>UneedIT biedt alle diensten op locatie aan.</p>
-        <p>Wij bieden de volgende diensten aan:</p>
-        <ul>
-            <li>Reparatie van computers en laptops: </li>
-            <a>wij doen zowel apple als windows en androit appareten</a>
-            <li>Installatie van software:</li>
-            <a>wij maken uw windows en apple laptop gebruiks klaar</a>
-        </ul>
-        <p>Wilt u meer informatie over onze diensten? Neem dan contact met ons op.</p>
-        <a> U kunt ook altijd bij ons langskomen: Zuidbaan 514
-            2841 MD Moordrecht (Gouwe Park):</a>
-        <p>U kunt ons ook bellen tussen 09:00 - 18:00: +3118 28 202 18</p>
+        <div class="info-text">
+            <h1>Zakelijk</h1>
+            <p>UneedIT biedt alle diensten op locatie aan.</p>
+            <p>Wij bieden de volgende diensten aan:</p>
+            <ul>
+                <li>Reparatie van computers en laptops: </li>
+                <a>wij doen zowel apple als windows en androit appareten</a>
+                <li>Installatie van software:</li>
+                <a>wij maken uw windows en apple laptop gebruiks klaar</a>
+            </ul>
+            <p>Wilt u meer informatie over onze diensten? Neem dan contact met ons op.</p>
+            <a> U kunt ook altijd bij ons langskomen: Zuidbaan 514
+                2841 MD Moordrecht (Gouwe Park):</a>
+            <p>U kunt ons ook bellen tussen 09:00 - 18:00: +3118 28 202 18</p>
+        </div>
+
+        <div class="adres">
+            <div id="locatie">
+                <p>Zuidbaan 514, 2841MD</p>
+                <p>Mordrecht</p>
+            </div>
+            <div id="berijkbaar">
+                <p> +316 30 985 409 SERVICENUMMER</p>
+                <p> +3118 28 202 18 KANTOOR</p>
+                <p>BEREIKBAAR VAN 09:00 - 18:00</p>
+            </div>
+            <div id="abonnement">
+                <p> MA T/M VRIJ, 09:00 - 23:00</p>
+                <p>TELEFONISCH BEREIKBAAR</p>
+                <p>VOOR ABONNEMENTHOUDERS</p>
+            </div>
+        </div>
+
+
     </div>
     <div class="formulier">
-        <form action="zakelijk.php" method="post">
-            <label for="naam">Naam:</label>
-            <input type="text" id="naam" name="naam" required>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-            <label for="telefoonnummer">Telefoonnummer:</label>
-            <input type="tel" id="telefoonnummer" name="telefoonnummer" required>
-            <label for="bericht">Bericht:</label>
-            <textarea id="bericht" name="bericht" required></textarea>
-            <input type="submit" value="Verstuur">
+        <div id="text">
+            <h2>Contactformulier</h2>
+            <p>laat ons weten wat er fout is met welk appataat en doen er wat aan.</p>
 
-            <?php
+            <form action="zakelijk.php" method="post">
+                <label for="naam">Naam:</label>
+                <input type="text" id="naam" name="naam" required>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <label for="telefoonnummer">Telefoonnummer:</label>
+                <input type="tel" id="telefoonnummer" name="telefoonnummer" required>
+                <label for="bericht">Probleem:</label>
+                <textarea id="bericht" name="bericht" required></textarea>
+                <input type="submit" value="Verstuur">
+            </form>
 
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-            error_reporting(E_ALL);
+        </div>
 
-            $query = $bd = new PDO('mysql:host=localhost;dbname=uneet-it',
-                'root', '');
+        <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
 
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                // Retrieve form data
-                $naam = $_POST['naam'];
-                $email = $_POST['email'];
-                $telefoonnummer = $_POST['telefoonnummer'];
-                $bericht = $_POST['bericht'];
+        $query = $bd = new PDO('mysql:host=localhost;dbname=uneet-it',
+            'root', '');
 
-                // Insert data into database
-                $query = $bd->prepare('INSERT INTO `formulier` (`naam`, `email`, `telefoonnummer`, `bericht`) VALUES (:naam, :email, :telefoonnummer, :bericht)');
-                $query->execute([
-                    'naam' => $naam,
-                    'email' => $email,
-                    'telefoonnummer' => $telefoonnummer,
-                    'bericht' => $bericht
-                ]);
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // ontvangen data
+            $naam = $_POST['naam'];
+            $email = $_POST['email'];
+            $telefoonnummer = $_POST['telefoonnummer'];
+            $bericht = $_POST['bericht'];
 
-                Header('Location: zakelijk.php');
-            }
-
-            ?>
+            // verstuur data naar database
+            $query = $bd->prepare('INSERT INTO `formulier` (`naam`, `email`, `telefoonnummer`, `bericht`) VALUES (:naam, :email, :telefoonnummer, :bericht)');
+            $query->execute([
+                'naam' => $naam,
+                'email' => $email,
+                'telefoonnummer' => $telefoonnummer,
+                'bericht' => $bericht
+            ]);
+            Header('Location: zakelijk.php');
+        }
+        ?>
         </form>
-
     </div>
+
 </main>
-<footer>
-    <p>&copy; UneedIT</p>
-</footer>
 </html>
