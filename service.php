8f0f3062478aaea9be244c50a819f19129db7adf
Roykramer <9023320@student.zadkine.nl>
2024-04-17 21:49:15 +0200

    php en css van service pagina af

--- service.php
+++ service.php
@@ -1,1 +1,93 @@
 <?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>uneed-it</title>
+    <link rel="stylesheet" href="Service.css"
+</head>
+<body>
+<nav>
+    <div class="nav">
+        <div class="navlogo">
+            <img src="logo.png" alt="uneed-it logo">
+        </div>
+        <div class="nav-links">
+            <a href="home.php">home</a>
+            <a href="itnieuws.php">it nieuws</a>
+            <a href="overons.php">over ons</a>
+            <a href="reparatie.php">reparatie</a>
+            <a href="service.php">service</a>
+            <a href="zakelijk.php">zakelijk</a>
+        </div>
+    </div>
+</nav>
+
+<div class="middel">
+<h1> Service pagina</h1>
+    <p> Op deze pagina vind u informatie betreft service </p>
+</div>
+
+<div class="contact-info">
+    <div class="contact-section">
+        <p>locatie:</p>
+        <p> zuidbaan 514, 2841MD moordrecht</p>
+    </div>
+    <div class="contact-section">
+        <p>Neem contact met ons op:</p>
+        <p>+316 30 985 409 servicenummer,
+            +3118 28 202 18 kantoor,
+            bereikbaar van 09:00 - 18:00</p>
+        <p>E-mail:</p>
+        <p> info@uneed-it.nl </p>
+    </div>
+    <div class="contact-section">
+        <p> Bezorgdiensten: </p>
+        <p> DHL </p>
+        <p> UPS </p>
+        <p> HOMERR </p>
+        <p> Alleen ophalen voor pakketten </p>
+    </div>
+</div>
+
+
+<div class="service">
+    <div class="service-information">
+        <h1> Service </h1>
+        <p> Wij van Uneed-IT zijn voor heel veel diensten beschikbaar.</p>
+        <p> Denk aan reparaties voor uw PC of onderhoud die gedaan moet worden.</p>
+        <p> Heeft uw vragen die niet bij de FAQ staan of specifieke vragen over uw PC,</p>
+        <p> Dan kunt u contact met ons opnemen via de contactgegevens die op de begin pagina staan.</p>
+        <p> En dan zullen wij u zo snel mogelijk van dienst zijn en het probleem of vraag te beantwoorden.</p>
+        <p> Wij van Uneed-IT werken met de bezorgdiensten; UPS, DHL en HOMERR.</p>
+        <p> </p>
+    </div>
+</div>
+
+<img src="uneedi01.jpg" class="image1">
+
+
+
+
+<footer>
+    <div class="contact-info">
+        <div class="contact-section2">
+            <p>locatie:</p>
+            <p> zuidbaan 514, 2841MD moordrecht</p>
+        </div>
+        <div class="contact-section2">
+            <p>Neem contact met ons op:</p>
+            <p>+316 30 985 409 servicenummer,
+                +3118 28 202 18 kantoor of email
+                bereikbaar van 09:00 - 18:00</p>
+        </div>
+        <div class="contact-section2">
+            <p>bezorg diensten:</p>
+            <p>UPS, DHL en HOMERR</p>
+        </div>
+    </div>
+</footer>
+
+</body>
+</html>
