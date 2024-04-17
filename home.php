fbb28dc0a399bbdd16edf8d19be09ae07fd03214
Roykramer <9023320@student.zadkine.nl>
2024-04-17 09:59:02 +0200

    Home page af

--- home.php
+++ home.php
@@ -5,9 +5,58 @@
 <head>
     <meta charset="UTF-8">
     <title>uneed-it</title>
+    <link rel="stylesheet" href="home.css">
 </head>
 <body>
+<nav>
+    <div class="nav">
+        <div class="navlogo">
+            <img src="logo.png" alt="uneed-it logo">
+        </div>
+        <div class="nav-links">
+            <a href="home.php">home</a>
+            <a href="itnieuws.php">it nieuws</a>
+            <a href="overons.php">over ons</a>
+            <a href="reparatie.php">reparatie</a>
+            <a href="service.php">service</a>
+            <a href="zakelijk.php">zakelijk</a>
+            <a href="#">Webshop</a>
+        </div>
+    </div>
+</nav>
+
+<div class="middel">
+    <h1>Welkom op de website van Uneed-IT</h1>
+    <p> Wij van Uneed-IT zijn een bedrijf dat gespecialiseerd is in de IT</p>
+    <p> met een historie op het gebied van PC reparaties en onderhoud.</p>
+   <br>
+    <p> Is er iets kapot aan uw PC of werkt hij niet meer?</p>
+    <p> Neem dan contact met ons op met de contactgegevens die onderaan de pagina staan.</p>
+    <p> Dan zullen wij u zo spoedig mogelijk van dienst zijn.</p>
+    
 
 </div>
+
+<div class="contact-info">
+    <div class="contact-section">
+        <p>locatie:</p>
+        <p> zuidbaan 514, 2841MD moordrecht</p>
+    </div>
+    <div class="contact-section">
+        <p>Neem contact met ons op:</p>
+        <p>+316 30 985 409 servicenummer,
+            +3118 28 202 18 kantoor,
+            bereikbaar van 09:00 - 18:00</p>
+        <p>E-mail:</p>
+        <p> info@uneed-it.nl</p>
+    </div>
+    <div class="contact-section">
+        <p> Bezorgdiensten: </p>
+        <p> DHL </p>
+        <p> UPS </p>
+        <p> HOMERR </p>
+    </div>
+</div>
+
 </body>
 </html>
